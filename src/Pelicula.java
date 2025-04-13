aa9b6e51feb11cdc9fce1f89fb09e0bd7f7f7726
jarvychavez <jav.chavez94@gmail.com>
2025-04-12 19:11:26 -0600

    ficha tecnica

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -3,6 +3,23 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    double sumaDeLasEvaluaciones;
+    int totalDeEvaluaciones;
+
+    void muestraFicheTecnica(){
+        System.out.println("El nombre de la película es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("La pelicula dura " + duracionEnMinutos);
+    }
+
+    void evalua (double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeEvaluaciones++;
+    }
+
+    double calculaLaMedia(){
+        return sumaDeLasEvaluaciones / totalDeEvaluaciones;
+    }
 
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -5,19 +5,24 @@ public class Principal {
         miPelicula.fechaDeLanzamiento = 2021;
         miPelicula.duracionEnMinutos = 120;
 
-        System.out.println("Mi película es:" + miPelicula.nombre);
-        System.out.println("Su fecha de lanzamiento es: " + miPelicula.fechaDeLanzamiento);
-        System.out.println("La pelicula dura " + miPelicula.duracionEnMinutos);
+        miPelicula.muestraFicheTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+
+        System.out.println(miPelicula.sumaDeLasEvaluaciones);
+        System.out.println(miPelicula.totalDeEvaluaciones);
+        System.out.println(miPelicula.calculaLaMedia());
+
+
 
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.nombre = "Matrix";
         otraPelicula.fechaDeLanzamiento = 1998;
         otraPelicula.duracionEnMinutos = 180;
 
-        System.out.println("Mi otra pelicula es: " + otraPelicula.nombre);
-        System.out.println("Su fecha de lanzamiento fue: " + otraPelicula.fechaDeLanzamiento);
-        System.out.println("Esta película dura " + otraPelicula.duracionEnMinutos);
-        
+        //otraPelicula.muestraFicheTecnica();
+
+
 
 
     }
