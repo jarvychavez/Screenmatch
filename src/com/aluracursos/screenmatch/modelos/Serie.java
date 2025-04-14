4a380c298c29539853c936f5af8655939d15f39f
jarvychavez <jav.chavez94@gmail.com>
2025-04-14 16:06:04 -0600

    añadimos la herencia y creamos una nueva clase llamada serie y titulo y todo lo de pelicula lo pegamos en titulo

---
+++ src/com/aluracursos/screenmatch/modelos/Serie.java
@@ -0,0 +1,31 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Serie extends Titulo {
+    int temporadas;
+    int episodioPorTemporada;
+    int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpisodioPorTemporada() {
+        return episodioPorTemporada;
+    }
+
+    public void setEpisodioPorTemporada(int episodioPorTemporada) {
+        this.episodioPorTemporada = episodioPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+}
---
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -0,0 +1,61 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Titulo {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeEvaluaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalDeEvaluaciones(){
+        return totalDeEvaluaciones;
+    }
+
+    public void muestraFichaTecnica(){
+        System.out.println("El nombre de la película es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("La pelicula dura: " + duracionEnMinutos);
+    }
+
+    public void evalua (double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeEvaluaciones++;
+    }
+
+    public double calculaLaMedia(){
+        return sumaDeLasEvaluaciones / totalDeEvaluaciones;
+    }
+}
