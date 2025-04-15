a8cba151518ebf9d4e62b2285e2cc303bec57882
jarvychavez <jav.chavez94@gmail.com>
2025-04-15 04:09:25 -0600

    le quite las importaciones de pelicula y serie a la calculadora de tiempo por que puse titulo y en ese vienen los 2

--- src/com/aluracursos/screenmatch/calculos/CalculadoraDeTiempo.java
+++ src/com/aluracursos/screenmatch/calculos/CalculadoraDeTiempo.java
@@ -1,6 +1,4 @@
 package com.aluracursos.screenmatch.calculos;
-import com.aluracursos.screenmatch.modelos.Pelicula;
-import com.aluracursos.screenmatch.modelos.Serie;
 import com.aluracursos.screenmatch.modelos.Titulo;
 
 public class CalculadoraDeTiempo {
