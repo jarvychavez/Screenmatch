76a74331a13dfd7d8a3502bfc05050b852c5d5c5
jarvychavez <jav.chavez94@gmail.com>
2025-05-01 20:14:40 -0600

    request y responds HTTP

---
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -0,0 +1,25 @@
+package com.aluracursos.screenmatch.principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class PrincipalConBusqueda {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?t=Matrix&plot=full&apikey=61f8a119"))
+                .build();
+
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+
+    }
+
+}
