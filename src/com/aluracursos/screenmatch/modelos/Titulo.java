ade7c930af005f972c4b14bd95ff4de937a7f3ad
jarvychavez <jav.chavez94@gmail.com>
2025-05-01 23:53:31 -0600

    agregamos la biblioteca de Gson

--- src/com/aluracursos/screenmatch/modelos/Titulo.java
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -1,7 +1,11 @@
 package com.aluracursos.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nombre;
+    @SerializedName("Year")
     private int fechaDeLanzamiento;
     private int duracionEnMinutos;
     private boolean incluidoEnElPlan;
@@ -67,5 +71,11 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo otroTitulo) {
         return this.getNombre().compareTo(otroTitulo.getNombre());
+    }
+
+    @Override
+    public String toString() {
+        return "nombre='" + nombre + '\'' +
+                ", fechaDeLanzamiento=" + fechaDeLanzamiento;
     }
 }
--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -1,11 +1,16 @@
 package com.aluracursos.screenmatch.principal;
 
+import com.aluracursos.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
+
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -24,7 +29,15 @@ public class PrincipalConBusqueda {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo miTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(miTitulo);
+
+
+
 
 
     }
