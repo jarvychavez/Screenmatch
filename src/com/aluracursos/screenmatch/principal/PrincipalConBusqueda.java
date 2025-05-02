2fc4b44140b1c8c171664eae4cda4d37463639b3
jarvychavez <jav.chavez94@gmail.com>
2025-05-01 20:37:08 -0600

    poner url con scanner para cualquier pelicula

--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -5,13 +5,20 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        Scanner lectura =new Scanner(System.in);
+        System.out.println("Escriba el nombre de una pelicula ");
+        var busqueda = lectura.nextLine();
+
+        String direccion = "https://www.omdbapi.com/?t="+busqueda+"&apikey=61f8a119";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=Matrix&plot=full&apikey=61f8a119"))
+                .uri(URI.create(direccion))
                 .build();
 
         HttpResponse<String> response = client
