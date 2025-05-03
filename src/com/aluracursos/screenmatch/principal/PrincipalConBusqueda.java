307cc0efbcc40c1ae2f8d31e62825178c48e2efb
jarvychavez <jav.chavez94@gmail.com>
2025-05-03 02:57:42 -0600

    viendo errores y exepciones con try y catch

--- src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -40,9 +40,16 @@ public class PrincipalConBusqueda {
                 .create();
         TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
         System.out.println(miTituloOmdb);
-        Titulo miTitulo = new Titulo(miTituloOmdb);
-        System.out.println(miTitulo);
 
+        try {
+            Titulo miTitulo = new Titulo(miTituloOmdb);
+            System.out.println(miTitulo);
+        }catch (NumberFormatException e){
+            System.out.println("Ocurrió un error ");
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Finalizo la ejecución del programa!");
 
     }
 
