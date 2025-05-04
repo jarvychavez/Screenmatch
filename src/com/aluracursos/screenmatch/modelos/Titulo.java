09b8d4f853e3e3c8d6f6fbd1a41ce0842b6149ee
jarvychavez <jav.chavez94@gmail.com>
2025-05-04 17:09:31 -0600

    agregar funcionalidad a las listas con json

--- src/com/aluracursos/screenmatch/modelos/Titulo.java
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -4,9 +4,7 @@ import com.aluracursos.screenmatch.excepcion.ErrorEnConversionDeDuracionExceptio
 import com.google.gson.annotations.SerializedName;
 
 public class Titulo implements Comparable<Titulo> {
-    @SerializedName("Title")
     private String nombre;
-    @SerializedName("Year")
     private int fechaDeLanzamiento;
     private int duracionEnMinutos;
     private boolean incluidoEnElPlan;
@@ -88,8 +86,8 @@ public class Titulo implements Comparable<Titulo> {
 
     @Override
     public String toString() {
-        return "nombre='" + nombre + '\'' +
+        return "(nombre='" + nombre +
                 ", fechaDeLanzamiento=" + fechaDeLanzamiento+
-                ", duración=" + duracionEnMinutos;
+                ", duración=" + duracionEnMinutos+")";
     }
 }
